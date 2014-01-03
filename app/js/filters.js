b7f0e35d3f9750342420cfde950faf0ee29d955c
Tim Geurts <geurts.tim@gmail.com>
2014-01-03 19:53:50 +0000

    more descriptive less/css filenames, tidied up console.logs in filters

--- app/js/filters.js
+++ app/js/filters.js
@@ -28,24 +28,6 @@ angular.module('portfolio.filters', [])
     };
   })
 
-  .filter('orderByDate', function(){
-   return function(input, date) {
-      if (!angular.isObject(input)) return input;
-
-      var array = [];
-      for(var objectKey in input) {
-          array.push(input[objectKey]);
-      }
-
-      array.sort(function(a, b){
-          a = parseInt(a[date]);
-          b = parseInt(b[date]);
-          return a - b;
-      });
-      return array;
-   }
-  })
-
   .filter('searchTag', function(){
     // Filter out posts that aren't released
     return function(assets, tag) {
